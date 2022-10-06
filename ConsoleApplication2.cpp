a993e10dc27587c6255e6f0d639c4eedef5dff67
Nikita Blinayev <nicklin411@gmail.com>
2022-10-06 05:36:46 +0300

    gg

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 #include <string>
 #include <cctype>
-#define ""
 using namespace std;
 
 template<typename T>
@@ -23,7 +22,6 @@ public:
     ~DynamicArrayStack()
     {
         delete[] stack;
-        delete[] dummy;
     }
     void pushStack(T value)
     {
@@ -41,12 +39,16 @@ public:
     }
     int popStack()
     {
-        if (top = -1)
+        if (top == -1)
         {
             return 0;
         }
-        max -= 1;
-        top -= 1;
+        else
+        {
+            max = max - 1;
+            top = top - 1;
+        }
+        return 1;
     }
     DynamicArrayStack(const DynamicArrayStack& st)
     {
@@ -91,30 +93,86 @@ public:
         }
         cout << endl;
     }
+    char lastStackElement()
+    {
+        return stack[top];
+    }
 };
 
-void rearrangeExpression(string &inBuff, string &outBuff, DynamicArrayStack <char> &stck)
+int symbPriority(char symb)
 {
+    switch (symb)
+    {
+    case '+':
+    case '-':
+        return 1;
+
+    case '*':
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    default:
+        return 0;
+    }
+}
+
+void rearrangeExpression(string &inBuff, string &outBuff, DynamicArrayStack <char> &stck)
+{   
     for (int i = 0; i < inBuff.length(); i++)
     {
         char c = inBuff[i];
-        if (isdigit(c) == true)
+        if (isdigit(c) != 0)
         {
             outBuff += c;
+            outBuff += " ";
         }
-        else
+        if (c == 's' || c == 'c')
         {
             stck.pushStack(c);
         }
+        if (c == '(')
+        {
+            stck.pushStack(c);
+        }
+        if (c == ')')
+        {
+            while (stck.lastStackElement() != '(')
+            {
+                outBuff += stck.lastStackElement();
+                outBuff += " ";
+                stck.popStack();
+            }
+            stck.popStack();
+        }
+        if (symbPriority(c) != 0)
+        {
+            while (stck.lastStackElement() == 's' || stck.lastStackElement() == 'c' || symbPriority(stck.lastStackElement()) >= symbPriority(c))
+            {
+                
+                outBuff += stck.lastStackElement();
+                outBuff += " ";
+                stck.popStack();
+            }
+            stck.pushStack(c);
+        }
     }
-}
+    while (stck.countStack() != 0)
+    {
+        outBuff += stck.lastStackElement();
+        outBuff += " ";
+        stck.popStack();
+    }
+}   
+
+// sin ( 2 ) + cos ( 5 ) * 2
+
 
 int main(int argc, char* argv[])
 {
     DynamicArrayStack <char> stackOne;
-    stackOne.printStack();
-    stackOne.pushStack('+');
-    stackOne.printStack();
-    
-}
-;
+    string str = "s ( 2 ) + c ( 5 ) * 2 ^ 5";
+    string outStr = "";
+    rearrangeExpression(str, outStr, stackOne);
+    cout << outStr << endl;
+};
